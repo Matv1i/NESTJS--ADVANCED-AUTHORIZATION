3204ce84e9a9a90086394211a9d93fe38d4f316b
Matv1i <matvey80po@gmail.com>
2024-12-22 23:20:10 +0100

    recaptcha

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Authorized } from 'src/decorators/authotizated.decorator';
 import { UserRole } from 'prisma/__generated__';
@@ -8,10 +8,17 @@ import { Authorization } from 'src/decorators/auth.decorator';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Authorization(UserRole.ADMIN)
+  @Authorization()
   @HttpCode(HttpStatus.OK)
   @Get('profile')
   public async findProfile(@Authorized('id') userId: string) {
     return this.userService.findById(userId);
+  }
+
+  @Authorization(UserRole.ADMIN)
+  @HttpCode(HttpStatus.OK)
+  @Get('by-id/:id')
+  public async findById(@Param('id') id: string) {
+    return this.userService.findById(id);
   }
 }
